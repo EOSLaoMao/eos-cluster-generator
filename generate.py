7bea64c34c84b942dfac28dc182bb3b69595cc3f
ZhaoYu <zhaoyu.johnny@gmail.com>
2018-08-15 15:28:33 +0900

    remove order version wallet

--- generate.py
+++ generate.py
@@ -175,6 +175,7 @@ def generate_sys_accounts():
 
 def generate_wallet_script():
     wallet_script = open(WALLET_SCRIPT, 'w')
+    wallet_script.write(cmd_wrapper("sh -c 'rm /opt/eosio/bin/data-dir/default.wallet' || true", CMD_PREFIX_KEOSD))
     wallet_script.write(cmd_wrapper("sh -c 'rm ~/eosio-wallet/default.wallet' || true", CMD_PREFIX_KEOSD))
     wallet_script.write(cmd_wrapper("cleos wallet create -n default > wallet_password", CMD_PREFIX_KEOSD))
     wallet_script.close()
