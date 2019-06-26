8a5c08d430d838c5bd82f39ea7563b8948351883
JohnnyZhao <zhaoyu.johnny@gmail.com>
2019-06-26 12:37:20 +0900

    boot.sh added

---
+++ boot.sh
@@ -0,0 +1,15 @@
+docker-compose up -d
+sleep 2
+./00_import_keys.sh
+sleep 2
+./01_create_token.sh
+sleep 2
+./02_create_accounts.sh
+sleep 2
+./03_reg_producers.sh
+sleep 2
+./04_issue_voter_token.sh
+sleep 2
+./05_delegate_voter_token.sh
+sleep 2
+./06_vote.sh
