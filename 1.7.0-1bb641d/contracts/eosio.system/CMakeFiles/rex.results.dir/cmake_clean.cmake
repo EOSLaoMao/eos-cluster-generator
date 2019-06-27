d7c9e494727867227ace1858eb92feca48e25f9a
JohnnyZhao <zhaoyu.johnny@gmail.com>
2019-06-27 17:08:00 +0900

    upgrade to work with 1.8.0-rc2

---
+++ 1.7.0-1bb641d/contracts/eosio.system/CMakeFiles/rex.results.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/rex.results.dir/src/rex.results.cpp.obj"
+  ".rex/rex.results.pdb"
+  ".rex/rex.results.wasm"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/rex.results.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
